719859619ba2ce21cfbcada2c050a7bbd2a1fb66
0vinod007 <vinod.mca14@gmail.com>
2017-06-28 12:01:09 +0530

    Added successfully

--- demo.java
+++ demo.java
@@ -1,12 +1,10 @@
-class JenkinsWithGit {
-
-	public static void main(String[] args) {
-		for(int i = 0 ; i <= 10;i++)
+class demo
+{
+	public static void main(String[] args )
+	{
+		for(int i=1;i<=10;i++)
 		{
-			System.out.println("Hello World ... "+i);
+			System.out.println("Hello World...." +i);
 		}
-			
-
 	}
-
 }
